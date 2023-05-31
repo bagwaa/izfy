e2ed8c69fd8254ebc01a36ea1be957c9145e7c1e
Richard Bagshaw <me@richardbagshaw.co.uk>
2023-05-31 22:14:58 +0100

    its fixed!

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
+import Confetti from "react-confetti";
+
 function App() {
-  function getDaysCounting(pastDate) {
-    const today = new Date();
-    const timeDiff = today.getTime() - pastDate.getTime();
-    const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    return daysDiff;
-  }
-
-  const pastDate = new Date("2023-05-24");
-  const daysSince = getDaysCounting(pastDate);
-
   return (
     <>
       <div className="grid h-screen place-items-center bg-black">
+        <Confetti />
         <div className="font-mono text-center">
-          <p className="text-9xl font-bold text-red-600">NO</p>
-          <p className="text-white">{daysSince} days and counting</p>
+          <p className="text-9xl font-bold text-green-600">YES</p>
+          <p className="text-white">Took only 9 DAYS!</p>
         </div>
       </div>
     </>
